d0b2f9f024676d3c2da7c28f30db1ab31666c8ba
Chika Pramulistia Putri <honeylemondrink44@gmail.com>
2026-09-17 12:16:51 +0800

    Hello World

--- posttests/test.py
+++ posttests/test.py
@@ -1,1 +1,28 @@
-print("Hello World");
+nama = "Chika"
+Nama = "TungTungTung Sahur"
+angka = not 1
+print(angka)
+
+namaPanjang = "Chika Pramulistia Putri"
+print(Nama)
+
+a = "Makan"
+b = "Nasi"
+print(a + b)
+
+print("Aku", "TungTungTung", "Sahur")
+
+teks = "Rahmat Flowchart"
+print(teks[7:])
+
+mata_kuliah1 = ["APD","Logika Matematika","Kalkulus"]
+mata_kuliah2 = {"APD", "Logika Matematika", "Kalkulus"}
+print(mata_kuliah1[0])
+print(mata_kuliah2)
+
+versi = 1
+versi = str(versi)
+print(versi * 2)
+
+nama = input("Masukan nama anda: ")
+print(nama)
